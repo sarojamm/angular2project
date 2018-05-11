d93fb8faf3ce4ef8a2888f7ada4cb3eaf0635200
Saroja Ammula <saroja.ammula@gmail.com>
2018-05-11 07:40:19 -0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { LicenseManager } from 'ag-grid-enterprise/main';
-LicenseManager.setLicenseKey('Lawrence_Livermore_National_Lab_WebPricer_3Devs15_June_2018__MTUyOTAxNzIwMDAwMA==f69b39cee122ad83fea1bbe23d5d886c');
 
 if (environment.production) {
   enableProdMode();
